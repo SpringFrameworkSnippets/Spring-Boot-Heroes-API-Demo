9e1fdbb8bdcf2852e936447492525e733730ce61
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2022-10-15 19:23:35 -0300

    Implemented entityToDomain mapper op

--- src/main/java/com/springsamples/heroesapi/mappers/HeroesMapper.java
+++ src/main/java/com/springsamples/heroesapi/mappers/HeroesMapper.java
@@ -1,10 +1,12 @@
 package com.springsamples.heroesapi.mappers;
 
 import com.springsamples.heroesapi.domain.Hero;
+import com.springsamples.heroesapi.repositories.entities.HeroEntity;
 import com.springsamples.heroesapi.web.model.HeroDto;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface HeroesMapper {
     HeroDto domainToDto(Hero domain);
+    Hero entityToDomain(HeroEntity entity);
 }
