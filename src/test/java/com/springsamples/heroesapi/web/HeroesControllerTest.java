8f6cef31069ec61f03e9431a643edba579bf4bf4
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2022-10-15 16:36:53 -0300

    Added findHeroes 200 OK not empty tst

--- src/test/java/com/springsamples/heroesapi/web/HeroesControllerTest.java
+++ src/test/java/com/springsamples/heroesapi/web/HeroesControllerTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static com.springsamples.heroesapi.constants.Web.BASE_URL;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,5 +36,14 @@ public class HeroesControllerTest {
     public void findHeroes_200() throws Exception {
         this.mockMvc.perform(get(BASE_URL))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Should get 200 OK not empty response")
+    public void findHeroes_200_NotEmpty() throws Exception {
+        this.mockMvc.perform(get(BASE_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", not(empty())));
     }
 }
