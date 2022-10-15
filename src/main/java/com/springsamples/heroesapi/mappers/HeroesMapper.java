c80b5ae83450b921b1e635f83d56570660eea6fa
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2022-10-15 17:57:40 -0300

    Implemented domainToDto heroes mapper method. Updated POM so mapstruct and lombok can work together

--- src/main/java/com/springsamples/heroesapi/mappers/HeroesMapper.java
+++ src/main/java/com/springsamples/heroesapi/mappers/HeroesMapper.java
@@ -1,7 +1,10 @@
 package com.springsamples.heroesapi.mappers;
 
+import com.springsamples.heroesapi.domain.Hero;
+import com.springsamples.heroesapi.web.model.HeroDto;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface HeroesMapper {
+    HeroDto domainToDto(Hero domain);
 }
