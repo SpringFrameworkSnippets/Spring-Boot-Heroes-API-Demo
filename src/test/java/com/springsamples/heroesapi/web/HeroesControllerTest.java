20cbf7b6f2872f45016281ca2d3c997883a1a0c2
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2022-10-15 16:25:37 -0300

    Added findHeroes 200 response tst

---
+++ src/test/java/com/springsamples/heroesapi/web/HeroesControllerTest.java
@@ -0,0 +1,36 @@
+package com.springsamples.heroesapi.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+public class HeroesControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    @DisplayName("Should get 200 OK response")
+    public void findHeroes_200() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+}
