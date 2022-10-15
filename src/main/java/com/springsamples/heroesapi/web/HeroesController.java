bbb49fb5e1d2de907e9bb7ac7277d75d6de77e68
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2022-10-15 17:02:20 -0300

    Implemented 200 hero list

--- src/main/java/com/springsamples/heroesapi/web/HeroesController.java
+++ src/main/java/com/springsamples/heroesapi/web/HeroesController.java
@@ -1,11 +1,13 @@
 package com.springsamples.heroesapi.web;
 
+import com.springsamples.heroesapi.web.model.HeroDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 import static com.springsamples.heroesapi.constants.Web.BASE_URL;
 
@@ -15,7 +17,10 @@ public class HeroesController {
 
     @GetMapping
     public ResponseEntity<?> heroes() {
-        var heroes = List.of("Superman", "batman");
+        var heroes = List.of(HeroDto.builder()
+                .id(UUID.randomUUID())
+                .name("Batman")
+                .build());
         return ResponseEntity.ok(heroes);
     }
 }
