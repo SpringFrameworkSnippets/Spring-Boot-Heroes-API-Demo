a11ef44d0f9f12de0487391bdce3084b7382c39d
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2022-10-15 17:44:14 -0300

    Added MapStruct dependency && Hero domain object tst

---
+++ src/test/java/com/springsamples/heroesapi/domain/HeroTest.java
@@ -0,0 +1,24 @@
+package com.springsamples.heroesapi.domain;
+
+import com.springsamples.heroesapi.web.model.HeroDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HeroTest {
+
+    @Test
+    @DisplayName("domain object should have id and name attrs")
+    void heroHasProperties() {
+        var id = UUID.randomUUID();
+        var name = "Batman";
+        var domain = new Hero(id, name);
+        assertAll("Has attributes",
+                () -> assertEquals(id, domain.getId()),
+                () -> assertEquals(name, domain.getName()));
+    }
+}
