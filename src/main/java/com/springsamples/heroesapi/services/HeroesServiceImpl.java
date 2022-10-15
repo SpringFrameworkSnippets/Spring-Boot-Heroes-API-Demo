13c725d1c124524124b70b5459f45d2aec5aa56e
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2022-10-15 18:57:31 -0300

    Added findAll not empty hero service tst

--- src/main/java/com/springsamples/heroesapi/services/HeroesServiceImpl.java
+++ src/main/java/com/springsamples/heroesapi/services/HeroesServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Service
 public class HeroesServiceImpl implements HeroesService {
+    //TODO we should get hero entities from some repository and map them out
     @Override
     public List<Hero> findAll() {
         return Collections.emptyList();
---
+++ src/test/java/com/springsamples/heroesapi/services/HeroesServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.springsamples.heroesapi.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith({SpringExtension.class})
+@ContextConfiguration(classes = HeroesServiceImpl.class)
+class HeroesServiceImplTest {
+
+    @Autowired
+    HeroesService service;
+
+    @BeforeEach
+    void beforeEach() {
+
+    }
+
+    @AfterEach
+    void afterEach() {
+
+    }
+
+    @Test
+    void findAll() {
+        var heroes = service.findAll();
+        assertThat(heroes).isNotEmpty();
+    }
+}
