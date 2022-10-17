f4cf6d002d828439d2175516ec0319a31f4b6b15
Barberis Leonardo <lbarberis.simtlix@supervielle.com.ar>
2022-10-17 02:52:00 -0300

    Added OpenAPI support

--- src/main/java/com/springsamples/heroesapi/HeroesApiApplication.java
+++ src/main/java/com/springsamples/heroesapi/HeroesApiApplication.java
@@ -1,9 +1,11 @@
 package com.springsamples.heroesapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class HeroesApiApplication {
 
     public static void main(String[] args) {
